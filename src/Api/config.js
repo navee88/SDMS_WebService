b198ade843754973d1be40c428a29a5cb0c17cb5
navee88 <naveen.k@agaramtech.com>
2025-12-19 11:11:47 +0530

    Change API base URL from 8094 to 9090

--- src/Api/config.js
+++ src/Api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
- baseURL:"http://localhost:8094/SDMS_WebService/",
+ baseURL:"http://localhost:9090/SDMS_WebService/",
   timeout: 60000
 });
 
