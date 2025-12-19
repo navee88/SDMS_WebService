fd0d148d9d0a867c3d084f7f61cd2a57bbd4ea17
navee88 <naveen.k@agaramtech.com>
2025-12-19 11:08:05 +0530

    Revert "Remove config.js from tracking"

---
+++ src/Api/config.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+ baseURL:"http://localhost:8094/SDMS_WebService/",
+  timeout: 60000
+});
+
+export const SECRET_KEY = "AGARAM_SDMS_SCRT";
+export const ITERATIONS = 100;
